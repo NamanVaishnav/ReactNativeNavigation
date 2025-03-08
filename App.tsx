1f1fe143ce116a78a142be5fa0b541ff5b756ba6
Naman Vaishnav <namanmvaishnav@gmail.com>
2025-03-08 17:27:49 +0530

    fix typescript error
    we need to explicitly define the type of each screen parameter. If a screen doesn't require parameters, use undefined.

--- App.tsx
+++ App.tsx
@@ -30,10 +30,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export type RootStackParams = {
-  Explore;
-  Restaurants;
-  Profile;
-
+  Explore: undefined;
+  Restaurants: undefined;
+  Profile: undefined;
 }
 
 const RootStack = createNativeStackNavigator<RootStackParams>();
